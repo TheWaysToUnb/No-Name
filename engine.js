c82db00c612cf0dfee9694e61ac565152a161921
TheWaysToUnb <apulliam605@gmail.com>
2026-06-11 15:47:23 -0400

    Update engine.js

--- engine.js
+++ engine.js
@@ -2,6 +2,8 @@ let currentShell = null;
 let account = null;
 let player;
 let input = { left: false, right: false, jump: false };
+let lastMeleeHitbox = null;
+
 
 window.onload = () => {
   account = loadAccount();
@@ -37,6 +39,11 @@ function startTestRoom() {
   const ctx = canvas.getContext('2d');
 
   player = new Player(currentShell, 200, 200);
+
+  spawnEnemy(500, 200);
+  spawnEnemy(700, 200);
+  spawnEnemy(900, 200);
+
 
   window.onkeydown = (e) => {
     if (e.key === 'a') input.left = true;
@@ -78,8 +85,41 @@ function startTestRoom() {
     requestAnimationFrame(loop);
 
     ACTIVE_PROJECTILES.forEach(p => { p.update(); p.draw(ctx); });
-ACTIVE_DEPLOYABLES.forEach(d => { d.update(); d.draw(ctx); });
-ACTIVE_DRONES.forEach(dr => { dr.update(); dr.draw(ctx); });
+    ACTIVE_DEPLOYABLES.forEach(d => { d.update(); d.draw(ctx); });
+    ACTIVE_DRONES.forEach(dr => { dr.update(); dr.draw(ctx); });
+
+    ACTIVE_ENEMIES.forEach(e => {
+    e.update(player);
+    e.draw(ctx);
+
+    // projectile collisions
+    ACTIVE_PROJECTILES.forEach((p, pi) => {
+    ACTIVE_ENEMIES.forEach((e) => {
+    if (p.x < e.x + e.width &&
+        p.x + p.size > e.x &&
+        p.y < e.y + e.height &&
+        p.y + p.size > e.y) {
+
+        e.takeDamage(p.damage);
+        ACTIVE_PROJECTILES.splice(pi, 1);
+    }
+  });
+});
+
+if (lastMeleeHitbox) {
+  ACTIVE_ENEMIES.forEach(e => {
+    if (lastMeleeHitbox.x < e.x + e.width &&
+        lastMeleeHitbox.x + lastMeleeHitbox.w > e.x &&
+        lastMeleeHitbox.y < e.y + e.height &&
+        lastMeleeHitbox.y + lastMeleeHitbox.h > e.y) {
+
+      e.takeDamage(12);
+    }
+  });
+}
+
+      
+});
 
   }
 
